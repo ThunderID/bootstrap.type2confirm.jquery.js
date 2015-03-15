35ae5418f9ca7400d3acce0b068a4d1fc693ddf5
erickmo <erick.mo@vortege.com>
2015-03-16 06:18:01 +0700

    add js

--- src/type2confirm.js
+++ src/type2confirm.js
@@ -5,16 +5,52 @@
  * Copyright (c) 2015 Erick Mo
  * Licensed under the MIT license.
  */
-(function ($) {
-  $.type2confirm = function (options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.type2confirm.options, options);
-    // Return the name of your plugin plus a punctuation character.
-    return 'type2confirm' + options.punctuation;
+(function($){
+  
+  $.fn.type2confirm = function(options) {
+    
+    var settings = $.extend({
+      text    : 'Delete',
+    }, options);
+
+    $(this).each(function(){
+      var id = Math.ceil(Math.random() * 10000);
+      $(this).attr('data-toggle', 'modal');
+      $(this).attr('data-target', '#type2confirmmodal-' + id);
+
+      $('body').append('<div class="modal fade" id="type2confirmmodal-'+id+'">' +
+                  '<div class="modal-dialog">             ' + 
+                    '<div class="modal-content">' + 
+                      '<div class="modal-header">' + 
+                      ' <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+                      ' <h4 class="modal-title">Delete Confirmation</h4>' + 
+                      '</div>' + 
+                      '<form method="'+$(this).attr('data-form-method')+'" action="'+$(this).attr('data-form-action')+'">' + 
+                      '<div class="modal-body">' + 
+                      ' This data will be deleted permanently. To confirm please type down <strong>'+settings.text+'</strong> ' + 
+                      ' the form provided below' + 
+                      ' <br/>' + 
+                      ' <input type="text" name="type2confirm" class="form-control">' + 
+                      '</div>' + 
+                      '<div class="modal-footer">' + 
+                      ' <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>' + 
+                      ' <button type="submit" class="btn btn-danger btn-confirm-delete" disabled>Yes, delete it!</button>' + 
+                      '</div>' + 
+                      '</form>' +
+                    '</div>' +  
+                  '</div>' + 
+                '</div>');
+      $('#type2confirmmodal-' + id).on('keyup', 'input[name=type2confirm]', function(){
+        if ($(this).val() != settings.text)
+        {
+          $('#type2confirmmodal-' + id).find('.btn-confirm-delete').attr('disabled', 'disabled');
+        }
+        else
+        {
+          $('#type2confirmmodal-' + id).find('.btn-confirm-delete').removeAttr('disabled');
+        }
+      });
+    });
   };
 
-  // Default options.
-  $.type2confirm.options = {
-    punctuation: '.'
-  };
-}(jQuery));
+})(jQuery);
